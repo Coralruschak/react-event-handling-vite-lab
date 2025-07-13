8a9f63067193c24322b00fd614bc7c12f808efee
Coral <coralruschak@gmail.com>
2025-07-12 23:42:46 -0400

    Creat function for entering & leaving of mouse

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,15 @@
 // Code SubmitButton Component Here
 
 function SubmitButton (){
+      function handleEnter(){
+        console.log("Mouse Entering")
+    }
+    function handleLeave() {
+        console.log("Mouse Exiting")
+    }
+
     return(
-        <></>
+        <button onMouseEnter={handleEnter} onMouseLeave={handleLeave}>Sumbit Password</button>
     )
 }
 
